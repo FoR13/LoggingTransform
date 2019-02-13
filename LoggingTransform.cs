5a2f7d458f98d54878b909ad70e121c7c297d18f
FoR <k.m.forest.x@gmail.com>
2019-02-13 20:52:47 +0900

    追加

---
+++ LoggingTransform.cs
@@ -0,0 +1,22 @@
+﻿//デバッグログにTransform情報を書き出すボダン
+//オブジェクトに貼ってLoggingを押す
+/////////////////////////
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoggingTransform : MonoBehaviour {
+
+    public bool Logging;
+    void OnValidate()
+    {
+        if (Logging)
+        {
+            Debug.Log(name +"\n"+ transform.position + "\n" + transform.eulerAngles + "\n" + transform.localScale );
+
+        }
+        Logging = false;
+        
+    }
+}
